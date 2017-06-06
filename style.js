6a714ee29735bdf1090abc9f4784a9810cc2ad0d
yunnabrus <yunnabrus@gmail.com>
2017-06-06 17:40:12 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,11 @@
+function leftAlert() {
+    alert('left');
+}
+
+function rightAlert() {
+    alert('rigth')
+}
+
+var item = document.getElementsById (latest-course-item-image);
+console.log(elem);
+
