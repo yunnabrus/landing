bf71e2a996d435d725e1c4a5b237c04b164a1ad0
yunnabrus <yunnabrus@gmail.com>
2017-06-26 17:48:09 +0300

    Add files via upload

--- style.js
+++ style.js
@@ -1,37 +1,11 @@
-console.log();
+$('.carousel').carousel()
 
+$('#weTab').tab()
 
-function leftAlert() {
-    alert('left');
-}
+$('#usTabs').tab()
 
-function rightAlert() {
-    alert('rigth')
-}
-
-var communityBlokThirdCircle = document.getElementById("communityBlokThirdCircle");
-communityBlokThirdCircle.onmouseover = function(){
- this.setAttribute("background-color:#2aba80","background-color:red;");
-}
-console.log(communityBlokThirdCircle);
-
-var courses = {
-    premier: 'url',
-    topic: 'text',
-    price: 75, 
-    isStarted: false,
-
-    getPriceWithDiscaunt: function() {
-        return price - 10;
-    },
-
-    startCourse: function() {
-        isStarted = true;
-    },
-}
-
-var coursesList = [ courses ];
-coursesList = someService.getLatestCourses();
+$('#myCollapsible').on('hidden.bs.collapse', function () {
+})
 
 
 
